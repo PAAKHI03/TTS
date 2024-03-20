27b5413f0bc01176420df4d126540f4e19aa4e95
PAAKHI MISHRA <162838705+PAAKHI03@users.noreply.github.com>
2024-03-20 16:43:34 +0530

    Update MyCustomVocab.cpp

--- MyCustomVocab.cpp
+++ MyCustomVocab.cpp
@@ -1,6 +1,6 @@
 #include "MyCustomVocab.h"
 
-const char paragraph1[] PROGMEM = "This is the first sentence of your paragraph. ";
-const char paragraph2[] PROGMEM = "This is the second sentence of your paragraph. ";
-const char paragraph3[] PROGMEM = "This is the third sentence of your paragraph. ";
+const char paragraph1[] PROGMEM = "This is the first sentence of my paragraph. ";
+const char paragraph2[] PROGMEM = "This is the second sentence of my paragraph. ";
+const char paragraph3[] PROGMEM = "This is the third sentence of my paragraph. ";
 
