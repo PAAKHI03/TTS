251bbbf4d80118d4579cc414a0250c144329c139
PAAKHI MISHRA <162838705+PAAKHI03@users.noreply.github.com>
2024-03-20 16:27:54 +0530

    Create MyCustomVocab.cpp

---
+++ MyCustomVocab.cpp
@@ -0,0 +1,7 @@
+//#include <avr/pgmspace.h>
+#include "MyCustomVocab.h"
+
+const char paragraph1[] PROGMEM = "This is the first sentence of your paragraph. ";
+const char paragraph2[] PROGMEM = "This is the second sentence of your paragraph. ";
+const char paragraph3[] PROGMEM = "This is the third sentence of your paragraph. ";
+
